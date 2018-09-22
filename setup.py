9f186cdae76cbdcccf3ed3e2372ab2a7a03a4f57
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-09-22 09:02:53 +0200

    Fix - Add readme and requirements in Manifest.in
    The package was not installable after the change for 1.2.2.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-
+from os import path
 import sys
 
-from setuptools import find_packages, setup
+import setuptools
 
 
 if sys.version_info < (2, 6):
     sys.exit('Sorry, Python < 2.6 is not supported')
+
+description = "A django survey app, based on and compatible with "
+"\"django-survey\". You will be able to migrate your data from an ancient "
+"version of django-survey, but it has been ported to python 3 and you can "
+"export results as CSV or PDF using your native language."
 
 
 def add_package(package_list, package):
@@ -13,20 +18,31 @@ def add_package(package_list, package):
     if package:
         package_list.append(package)
 
-setup(
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as fh:
+    require = fh.readlines()
+require = [x.strip() for x in require]
+
+with open("requirements_dev.txt", "r") as fh:
+    extras_require = fh.readlines()
+# Remove the first two line (-r requirements.txt and a blank line)
+extras_require = {'dev': [x.strip() for x in extras_require[2:]]}
+
+setuptools.setup(
     name="django-survey-and-report",
-    version="1.2.1",
-    description="A django survey app, based on and compatible with "
-                "'django-survey'",
-    long_description="A django survey app, based on and compatible with \"django-survey\"."
-"You will be able to migrate your data from an ancient version of "
-"django-survey, but you can use python 3 and export results as "
-"CSV or PDF using your native language.",
+    version="1.2.3",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Pierre SASSOULAS",
     author_email="pierre.sassoulas@gmail.com",
     license="AGPL",
     url="https://github.com/Pierre-Sassoulas/django-survey",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -41,12 +57,6 @@ setup(
         'Programming Language :: Python :: 3',
         "Framework :: Django",
     ],
-    install_requires=[
-        "Django", "django-bootstrap-form", "django-tastypie",
-        "django-registration", "pytz", "future", "ordereddict", "PyYAML",
-        "matplotlib", "seaborn", "numpy"
-    ],
-    extras_require={
-        'dev': ["django-rosetta", "pylint", "coverage", "mock"],
-    },
+    install_requires=require,
+    extras_require=extras_require,
 )
